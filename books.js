8d99c264438fcdba8b7991056e676ae407ce1fb0
nadareffat98 <eng.nada.reffat@gmail.com>
2022-06-01 16:40:47 -0700

    book store

---
+++ books.js
@@ -0,0 +1,109 @@
+let books = [
+    {
+        isbn: '1',
+        name:'Deception point',
+        category: 'drama',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/5/3/6/6/1001004011806635.jpg',
+        numOfpages: 45,
+        author: 'ahmed',
+        price: 205,
+        isFave:false,
+
+
+    },
+    {
+        isbn: '2',
+        name:'Magic staff',
+        category: 'crime',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/8/8/9/8/1001004006878988.jpg',
+        numOfpages: 890,
+        author: 'Terry Brooks',
+        price: 399,
+        isFave:false
+
+
+    },
+    {
+        isbn: '3',
+        name:'Bloodfire Quest',
+        category: 'drama',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/2/2/5/2/9200000002212522.jpg',
+        numOfpages: 700,
+        author: 'Terry Brooks',
+        price: 599,
+        isFave:false
+
+
+    },
+    {
+        isbn: '4',
+        name:'A Week in Winter',
+        category: 'crime',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/6/2/8/0/9200000008070826.jpg',
+        numOfpages: 200,
+        author: 'Maeve Binchy',
+        price: 550,
+        isFave:false
+
+    },
+    {
+        isbn: '5',
+        name:'Blue Curacao',
+        category: 'drama',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/0/9/4/2/9200000010732490.jpg',
+        numOfpages: 20,
+        author: 'Linda van Rijn',
+        price: 99,
+        isFave:false
+
+
+    },
+    {
+        isbn: '6',
+        name:'Safe Haven',
+        category: 'crime',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/6/3/5/1/1001004010981536.jpg',
+        numOfpages: 890,
+        author: 'Nicholas Sparks',
+        price: 399,
+        isFave:false
+
+
+    },
+    {
+        isbn: '7',
+        name:'De wereld volgens Garp',
+        category: 'drama',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/6/3/6/7/1001004005997636.jpg',
+        numOfpages: 700,
+        author: 'John Irving',
+        price: 599,
+        isFave:false
+
+
+    },
+    {
+        isbn: '8',
+        name:'Winter Chalet',
+        category: 'crime',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/6/6/6/7/9200000002547666.jpg',
+        numOfpages: 200,
+        author: 'Linda van Rijn',
+        price: 550,
+        isFave:false
+
+    },
+    {
+        isbn: '9',
+        name:'De delta deceptie',
+        category: 'romance',
+        image: 'http://s.s-bol.com/imgbase0/imagebase/large/FC/5/0/5/5/1001004006225505.jpg',
+        numOfpages: 600,
+        author: 'Linda van Rijn',
+        price: 666,
+        isFave:false
+
+    },
+
+
+];
